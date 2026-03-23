894a198dd3c8ba4d0b2db8f202b73173931413e8
Tushar Rathod <itstushar8208@gmail.com>
2026-03-23 08:35:55 +0530

    Time: 0 ms (100%), Space: 42.9 MB (79.89%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,17 @@
  class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.trim();
-        return s.length() - s.lastIndexOf(' ') - 1;
+        int length = 0;
+        int i = s.length() - 1;
+
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
     }
 }
